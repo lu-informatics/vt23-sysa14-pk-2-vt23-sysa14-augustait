7b65660ff1311fbcdd4440485778537cb1545b00
AdamJacobson3 <jacobsonadam10@gmail.com>
2023-02-10 16:17:24 +0000

    Add-metoden inlagd och funkar med SQL

--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -1,5 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonDb personDb = new PersonDb ("Data Source=localhost;Initial Catalog=Lidl;User ID=UserLidl;Password=123;Encrypt=False;TrustServerCertificate=True;");
 
-Console.WriteLine("Test");
+            Console.WriteLine("Enter the ID:");
+            int id = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the first name:");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter the last name:");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Enter the birth date (yyyy-MM-dd):");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+
+            personDb.AddPerson(id, firstName, lastName, birthDate);
+
+            Console.WriteLine("Person added successfully!");
+        }
+    }
+}
