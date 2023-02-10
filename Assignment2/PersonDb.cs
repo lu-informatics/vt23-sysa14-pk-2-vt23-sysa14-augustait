981c1bb5d02f16b6618c73d139791a47d1649f37
AdamJacobson3 <jacobsonadam10@gmail.com>
2023-02-10 15:42:59 +0000

    Skapat motsvarande DAL

---
+++ Assignment2/PersonDb.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    public class PersonDb
+    {
+        private string _connectionString;
+
+        public PersonDb(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void AddPerson(int id, string firstName, string lastName, DateTime birthDate)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand("INSERT INTO Person (id, first_name, last_name, birth_date) VALUES (@id, @firstName, @lastName, @birthDate)", connection))
+                {
+                    command.Parameters.AddWithValue("@id", id);
+                    command.Parameters.AddWithValue("@firstName", firstName);
+                    command.Parameters.AddWithValue("@lastName", lastName);
+                    command.Parameters.AddWithValue("@birthDate", birthDate);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
